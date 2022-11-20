94e73d22176099778ad41ef8482fce417bf7ce9c
ArunArun2326 <118500449+ArunArun2326@users.noreply.github.com>
2022-11-20 13:15:09 +0530

    Add files via upload

---
+++ CompanyInfo.java
@@ -0,0 +1,21 @@
+package org.company;
+
+public class CompanyInfo {
+	private void companyName() {
+	System.out.println("COMPANY NAME");
+	}
+	private void companyId() {
+		// TODO Auto-generated method stub
+System.out.println("COMPANY ID");
+	}
+	private void companyAddress() {
+		// TODO Auto-generated method stub
+System.out.println("COMPANY ADDRESS");
+	}
+	public static void main(String[] args) {
+		CompanyInfo abc = new CompanyInfo();
+		abc.companyName();
+		abc.companyId();
+		abc.companyAddress();
+	}
+}
